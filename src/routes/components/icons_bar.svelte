054f091a4991af92f0fe1f8b9bd2823932e02564
rachelkd <rachel05deng@gmail.com>
2023-07-03 21:12:18 -0700

    home page

---
+++ src/routes/components/icons_bar.svelte
@@ -0,0 +1,5 @@
+<button class="icon-btn"><i class="material-icons">email</i></button>
+<button class="icon-btn"><i class="material-icons">githubicon</i></button>
+<button class="icon-btn"><i class="material-icons">linkedin</i></button>
+
+
