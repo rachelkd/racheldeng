b85e6571bc5723b033e7be90d222bc77538d7114
rachelkd <rachel05deng@gmail.com>
2023-07-04 03:13:11 -0700

    Redo

--- src/routes/components/icons_bar.svelte
+++ src/routes/components/icons_bar.svelte
@@ -1,5 +1,15 @@
-<button class="icon-btn"><i class="material-icons">email</i></button>
-<button class="icon-btn"><i class="material-icons">githubicon</i></button>
-<button class="icon-btn"><i class="material-icons">linkedin</i></button>
+<script>
+    // import IconButton from '@smui/icon-button';
+    // @ts-ignore
+    import SvgIcon from '@jamescoyle/svelte-icon';
+    // @ts-ignore
+    import { mdiAccount } from '@mdi/js';
+</script>
 
+
+<SvgIcon type="mdi" path={mdiAccount}></SvgIcon>
+
+<button class="icon-btn"><i class="material-icons">email</i></button>
+<button class="icon-btn"><i class="material-icons">hub</i></button>
+<button class="icon-btn"><i class="material-icons">link</i></button>
 
