c8fd02b861d37dd00b202ef81d7bcba296962ad2
rachelkd <rachel05deng@gmail.com>
2023-07-03 21:11:51 -0700

    home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Icons from "./components/icons_bar.svelte";
+</script>
+<h1>nice to meet you! <br> my name is <span class=highlight>rachel</span>.</h1>
+<Icons />
